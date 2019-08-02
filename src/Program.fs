458c437f81ef33def9307c4fbfc50e84f1a94942
Petr Chromec <mortal_flesh@seznam.cz>
2019-08-02 13:15:19 +0200

    Add translation replace command

--- src/Program.fs
+++ src/Program.fs
@@ -4,5 +4,16 @@ open MF.ConsoleApplication
 let main argv =
     consoleApplication {
         title "Translations"
+
+        command "translation:replace" {
+            Description = "Replaces the texts with proper translations."
+            Help = None
+            Arguments = []
+            Options = []
+            Initialize = None
+            Interact = None
+            Execute = fun (input, output) ->
+                ExitCode.Success
+        }
     }
     |> run argv
