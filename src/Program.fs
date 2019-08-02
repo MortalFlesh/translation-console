2de2139a61dddc93b9096b4b8da438ba2724645f
Petr Chromec <mortal_flesh@seznam.cz>
2019-08-02 14:29:24 +0200

    Load files contents in dirs by extensions

--- src/Program.fs
+++ src/Program.fs
@@ -1,18 +1,43 @@
 open MF.ConsoleApplication
+open MF.TranslationConsole
 
 [<EntryPoint>]
 let main argv =
     consoleApplication {
         title "Translations"
+        info ApplicationInfo.MainTitle
 
         command "translation:replace" {
             Description = "Replaces the texts with proper translations."
             Help = None
-            Arguments = []
-            Options = []
+            Arguments = [
+                Argument.requiredArray "dirs" "Directories you want to search files."
+            ]
+            Options = [
+                Option.requiredArray "extension" (Some "e") "File extension to be filtered." None
+            ]
             Initialize = None
             Interact = None
             Execute = fun (input, output) ->
+                let toOption = List.map (fun value -> value, "")
+
+                let dirs = input |> Input.getArgumentValueAsList "dirs"
+                let extensions = input |> Input.getOptionValueAsList "extension"
+
+                let files = File.loadAllFileContents dirs extensions
+
+                if output.IsVerbose() then
+                    dirs |> toOption |> output.Options "Directories:"
+                    extensions |> toOption |> output.Options "Extensions:"
+
+                    files
+                    |> List.map (fun (file, contents) -> [
+                        file
+                        contents |> List.length |> string
+                    ])
+                    |> output.Table ["File"; "Lines"]
+
+                output.Success "Done"
                 ExitCode.Success
         }
     }
--- src/Utils.fs
+++ src/Utils.fs
@@ -1,2 +1,80 @@
 namespace MF.TranslationConsole
 
+[<AutoOpen>]
+module internal Utils =
+    let tee f a =
+        f a
+        a
+
+[<RequireQualifiedAccess>]
+module internal Async =
+    /// Lift a function to Async
+    let map f xA =
+        async {
+        let! x = xA
+        return f x
+        }
+
+    /// Lift a value to Async
+    let retn x =
+        async.Return x
+
+    /// Apply an Async function to an Async value
+    let apply fA xA =
+        async {
+         // start the two asyncs in parallel
+        let! fChild = Async.StartChild fA  // run in parallel
+        let! x = xA
+        // wait for the result of the first one
+        let! f = fChild
+        return f x
+        }
+
+    /// Apply a monadic function to an Async value
+    let bind f xA = async.Bind(xA,f)
+
+
+
+module File =
+    open System.IO
+
+    type FilePath = string
+
+    let private hasExtension (name: FilePath) (extension: string) =
+        name.EndsWith(extension)
+
+    let private fileHasExtension extensions name =
+        match extensions with
+        | [] -> true
+        | extensions ->
+            extensions
+            |> List.exists (name |> hasExtension)
+
+    let findAll (dirs: string list) (extensions: string list): FilePath list =
+        let rec getAllFiles = function
+            | [] -> []
+            | directories -> [
+                yield! directories |> Seq.collect Directory.EnumerateFiles |> Seq.filter (fileHasExtension extensions)
+                yield! directories |> Seq.collect Directory.EnumerateDirectories |> List.ofSeq |> getAllFiles
+            ]
+
+        getAllFiles dirs
+
+    type Lines = string list
+
+    let private readAllLinesAsync file: Async<FilePath * Lines> =
+        file
+        |> File.ReadAllLinesAsync
+        |> Async.AwaitTask
+        |> Async.map (fun lines -> file, lines |> Array.toList)
+
+    let loadContents (files: FilePath list) =
+        files
+        |> List.map readAllLinesAsync
+        |> Async.Parallel
+
+    let loadAllFileContents dirs extensions =
+        findAll dirs extensions
+        |> loadContents
+        |> Async.RunSynchronously
+        |> Array.toList
